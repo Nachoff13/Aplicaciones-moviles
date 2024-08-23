dcd08a37e08b37670df02b5db3b252cf96b9ef78
Sebastian Gonzales <sebastian.gt.2002@gmail.com>
2024-08-22 22:52:30 -0300

    fix: Cambio el nombre de la variable a minus

--- Trabajos Practicos/TP1 - Login/app/src/main/java/moviles/utnfrlp/login/MainActivity.kt
+++ Trabajos Practicos/TP1 - Login/app/src/main/java/moviles/utnfrlp/login/MainActivity.kt
@@ -12,7 +12,8 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     // Definir una etiqueta para los logs
-    private val TAG = "MainActivity"
+    private val tag = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -25,16 +26,24 @@ class MainActivity : AppCompatActivity() {
 
         val bIniciarSesion = findViewById<Button>(R.id.bIniciarSesion)
         bIniciarSesion.setOnClickListener{
+
             val etUsuario = findViewById<EditText>(R.id.etUsuario).text.toString()
+
             val etContrasena = findViewById<EditText>(R.id.etContrasena).text.toString()
+
             // Agregar logs para depuración
-            Log.d(TAG, "Usuario ingresado: $etUsuario")
-            Log.d(TAG, "Contraseña ingresada: $etContrasena")
+
+            Log.d(tag, "Usuario ingresado: $etUsuario")
+            Log.d(tag, "Contraseña ingresada: $etContrasena")
+
             if (etUsuario == "Juan Torres" && etContrasena == "1234utn"){
-                Log.i(TAG, "Login correcto")
+
+                Log.i(tag, "Login correcto")
+
                 Toast.makeText(this, "Login correcto", Toast.LENGTH_SHORT).show()
             } else {
-                Log.e(TAG, "Usuario o contraseña incorrectos")
+                Log.e(tag, "Usuario o contraseña incorrectos")
+
                 Toast.makeText(this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show()
             }
         }
