6622c566251a648f980f887985f78e53dae99738
Manuel Morullo <manuelmorullo@hotmail.com>
2024-08-23 00:01:16 -0300

    fix: creo activity Register

--- Trabajos Practicos/TP1 - Login/app/src/main/java/moviles/utnfrlp/login/RegisterActivity.kt
+++ Trabajos Practicos/TP1 - Login/app/src/main/java/moviles/utnfrlp/login/RegisterActivity.kt
@@ -9,5 +9,12 @@ import androidx.core.view.WindowInsetsCompat
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
