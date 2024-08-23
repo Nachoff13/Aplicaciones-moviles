81937205bffcb92a2f38adb0d69eb78c7f4dfc7a
Sebastian Gonzales <sebastian.gt.2002@gmail.com>
2024-08-22 22:51:38 -0300

    feat: Agrego la RegisterActivity en kt

---
+++ Trabajos Practicos/TP1 - Login/app/src/main/java/moviles/utnfrlp/login/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package moviles.utnfrlp.login
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
