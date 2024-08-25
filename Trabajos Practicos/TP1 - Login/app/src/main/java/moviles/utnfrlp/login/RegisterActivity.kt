fa09322236ecd5b9f30f1cea7ac1614a923ee781
Manuel Morullo <manuelmorullo@hotmail.com>
2024-08-25 18:54:05 -0300

    fix: elimino import que no se usa

--- Trabajos Practicos/TP1 - Login/app/src/main/java/moviles/utnfrlp/login/RegisterActivity.kt
+++ Trabajos Practicos/TP1 - Login/app/src/main/java/moviles/utnfrlp/login/RegisterActivity.kt
@@ -2,7 +2,6 @@ package moviles.utnfrlp.login
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
