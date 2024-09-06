633ce9fece666865abe4c31f93ebb9825d140bf4
Manuel Morullo <manuelmorullo@hotmail.com>
2024-09-05 22:24:58 -0300

    fix: elimino imports no utilizados

--- Trabajos Practicos/TP2 - APP 1/app/src/main/java/com/example/tp2_app1/MainActivity.kt
+++ Trabajos Practicos/TP2 - APP 1/app/src/main/java/com/example/tp2_app1/MainActivity.kt
@@ -7,11 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.tp2_app1.ui.theme.TP2APP1Theme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
