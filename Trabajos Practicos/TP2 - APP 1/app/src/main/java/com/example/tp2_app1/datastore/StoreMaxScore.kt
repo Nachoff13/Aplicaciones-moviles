7fc012571a4e0ff04db0252dbe4a31c02d272678
Sebastian Gonzales <sebastian.gt.2002@gmail.com>
2024-09-12 23:33:59 -0300

    feature: Agrego la configuración para guardar el puntaje maximo

--- Trabajos Practicos/TP2 - APP 1/app/src/main/java/com/example/tp2_app1/datastore/StoreMaxScore.kt
+++ Trabajos Practicos/TP2 - APP 1/app/src/main/java/com/example/tp2_app1/datastore/StoreMaxScore.kt
@@ -1,4 +1,32 @@
 package com.example.tp2_app1.datastore
 
-class StoreMaxScore {
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class StoreMaxScore(private val context: Context) {
+
+    // Única instancia de la clase
+    companion object {
+        private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "maxScore")
+        val MAX_SCORE_KEY = intPreferencesKey("max_score")
+    }
+
+    // Obtener el puntaje
+    val getScore: Flow<Int> = context.dataStore.data
+        .map { preferences ->
+        preferences[MAX_SCORE_KEY] ?: 0
+    }
+
+    // Establecer el puntaje
+    suspend fun setScore(score: Int) {
+        context.dataStore.edit { preferences ->
+            preferences[MAX_SCORE_KEY] = score
+        }
+    }
 }
