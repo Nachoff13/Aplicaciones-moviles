2d5d102bdc079ff073c0b2ab852d1e5ff1a4f4db
Juan Ignacio Frangolini <nachofrangolini@hotmail.com>
2024-09-20 17:49:41 -0300

    feat: agrego clases complementarias para la clase City

--- Trabajos Practicos/TP2APP2/app/src/main/java/com/example/tp2_app2/home/CityDao.kt
+++ Trabajos Practicos/TP2APP2/app/src/main/java/com/example/tp2_app2/home/CityDao.kt
@@ -1,4 +1,4 @@
-package com.example.tp2_app2
+package com.example.tp2_app2.home
 
 import androidx.room.Dao
 import androidx.room.Delete
--- Trabajos Practicos/TP2APP2/app/src/main/java/com/example/tp2_app2/home/CityDatabase.kt
+++ Trabajos Practicos/TP2APP2/app/src/main/java/com/example/tp2_app2/home/CityDatabase.kt
@@ -1,4 +1,4 @@
-package com.example.tp2_app2
+package com.example.tp2_app2.home
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
