10f3acddaa5518d22c9d863deb7617da9893f7ef
Juan Ignacio Frangolini <nachofrangolini@hotmail.com>
2024-09-20 17:47:02 -0300

    feat: agrego el item de city

---
+++ Trabajos Practicos/TP2APP2/app/src/main/java/com/example/tp2_app2/home/CityItem.kt
@@ -0,0 +1,34 @@
+package com.example.tp2_app2.home
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CityItem(
+    city: City,
+    onEdit: () -> Unit,
+    onDelete: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(text = "${city.name} $${city.population}")
+        IconButton(onClick = onEdit) {
+            Icon(imageVector = Icons.Default.Edit, contentDescription = "Edit")
+        }
+        IconButton(onClick = onDelete) {
+            Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete")
+        }
+    }
+}
