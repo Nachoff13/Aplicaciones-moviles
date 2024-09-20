a42df42426b16b60ab858d43cab33560835ef311
Juan Ignacio Frangolini <nachofrangolini@hotmail.com>
2024-09-20 17:49:04 -0300

    fix: arreglo las variables

--- Trabajos Practicos/TP2APP2/app/src/main/java/com/example/tp2_app2/home/Country.kt
+++ Trabajos Practicos/TP2APP2/app/src/main/java/com/example/tp2_app2/home/Country.kt
@@ -1,4 +1,4 @@
-package com.example.tp2_app2
+package com.example.tp2_app2.home
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 @Entity
 data class Country (
     @PrimaryKey(autoGenerate = true)
-    val countryId: String,
+    val countryId: Int,
     val name: String,
     val population: Int,
 
