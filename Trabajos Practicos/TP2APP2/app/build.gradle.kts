aa9bbe24ca0116ad3150089b29a054eb97f8fbfc
Manuel Morullo <manuelmorullo@hotmail.com>
2024-09-23 22:23:14 -0300

    feat: agrego dependencia

--- Trabajos Practicos/TP2APP2/app/build.gradle.kts
+++ Trabajos Practicos/TP2APP2/app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     // Room
     implementation(libs.androidx.room.ktx)
     implementation(libs.androidx.navigation.runtime.ktx)
+    implementation(libs.androidx.navigation.compose)
     kapt(libs.androidx.room.compiler)
 
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
