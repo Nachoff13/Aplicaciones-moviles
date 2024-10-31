25ec4edd311d491388d9056386aa0b801686e0a8
Manuel Morullo <manuelmorullo@hotmail.com>
2024-10-31 04:08:22 -0300

    feat: agrego mensaje de bienvenida

--- Trabajos Practicos/TP3/Habitos/app/(tabs)/home.tsx
+++ Trabajos Practicos/TP3/Habitos/app/(tabs)/home.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
-import { useRouter } from 'expo-router';
+import { View, Text, StyleSheet } from 'react-native';
+import { useLocalSearchParams } from 'expo-router';
 
-const Home = () => {
-  const router = useRouter();
-
-  const handleLogout = () => {
-    router.push('/login');
-  };
+const HomeScreen = () => {
+  const { userName, email } = useLocalSearchParams();
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Welcome to Your Home Screen!</Text>
-      <Text style={styles.subtitle}>You are now logged in.</Text>
-      <Button title="Logout" onPress={handleLogout} />
+      <Text style={styles.welcomeText}>
+        Bienvenido {userName} ({email})
+      </Text>
     </View>
   );
 };
@@ -23,19 +19,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 16,
-    backgroundColor: '#f5f5f5', // Color de fondo
+    backgroundColor: '#f7f7f7',
   },
-  title: {
+  welcomeText: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 8,
-  },
-  subtitle: {
-    fontSize: 16,
-    color: '#666',
-    marginBottom: 16,
   },
 });
 
-export default Home;
+export default HomeScreen;
