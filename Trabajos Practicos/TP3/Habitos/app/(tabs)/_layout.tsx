1b57f34a0f88dba5837d47483550338a3eb5cb7c
Manuel Morullo <manuelmorullo@hotmail.com>
2024-10-31 18:48:29 -0300

    feat: implemento navbar

--- Trabajos Practicos/TP3/Habitos/app/(tabs)/_layout.tsx
+++ Trabajos Practicos/TP3/Habitos/app/(tabs)/_layout.tsx
@@ -1,14 +1,28 @@
 import { Stack } from 'expo-router';
-import { AuthProvider } from '../../context/AuthContext';
+import { AuthProvider, useAuth } from '../../context/AuthContext';
+import Navbar from '../../components/navigation/Navbar';
 
 export default function AppLayout() {
   return (
     <AuthProvider>
+      <AuthenticatedApp />
+    </AuthProvider>
+  );
+}
+
+function AuthenticatedApp() {
+  const { email } = useAuth();
+
+  return (
+    <>
+      {/* Muestra el Navbar solo si el usuario está autenticado */}
+      {email && <Navbar />}
+      
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="login" />
         <Stack.Screen name="home" />
         <Stack.Screen name="register" />
       </Stack>
-    </AuthProvider>
+    </>
   );
 }
