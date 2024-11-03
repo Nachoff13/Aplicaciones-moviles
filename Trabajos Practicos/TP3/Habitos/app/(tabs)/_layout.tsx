ce8dce7b96cef4b5594363026fb53d250c715122
Manuel Morullo <manuelmorullo@hotmail.com>
2024-11-03 01:28:22 -0300

    feat: agrego proteccion de rutas privadas

--- Trabajos Practicos/TP3/Habitos/app/(tabs)/_layout.tsx
+++ Trabajos Practicos/TP3/Habitos/app/(tabs)/_layout.tsx
@@ -9,34 +9,47 @@ import AddHabitScreen from './addHabit';
 import DetailHabitScreen from './detailHabit';
 import ListHabitScreen from './listHabit';
 import { Provider as PaperProvider } from 'react-native-paper';
+import PrivateRoute from './privateRoute';
 
 const Stack = createStackNavigator();
 
 export default function AppLayout() {
   return (
     <PaperProvider>
-    <AuthProvider>
+      <AuthProvider>
         <AuthenticatedApp />
-    </AuthProvider>
+      </AuthProvider>
     </PaperProvider>
   );
 }
 
 function AuthenticatedApp() {
-  const { email } = useAuth();
+  const { isAuthenticated } = useAuth();
 
   return (
     <>
-      {/* Muestra el Navbar solo si el usuario está autenticado */}
-      {email && <Navbar />}
+      {isAuthenticated && <Navbar />}
       
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="login" component={LoginScreen} />
-        <Stack.Screen name="home" component={HomeScreen} />
         <Stack.Screen name="register" component={RegisterScreen} />
-        <Stack.Screen name="addHabit" component={AddHabitScreen} />
-        <Stack.Screen name="detailHabit" component={DetailHabitScreen} />
-        <Stack.Screen name="listHabit" component={ListHabitScreen} />
+        
+        <Stack.Screen 
+          name="home" 
+          component={() => <PrivateRoute component={HomeScreen} />} 
+        />
+        <Stack.Screen 
+          name="addHabit" 
+          component={() => <PrivateRoute component={AddHabitScreen} />} 
+        />
+        <Stack.Screen 
+          name="detailHabit" 
+          component={() => <PrivateRoute component={DetailHabitScreen} />} 
+        />
+        <Stack.Screen 
+          name="listHabit" 
+          component={() => <PrivateRoute component={ListHabitScreen} />} 
+        />
       </Stack.Navigator>
     </>
   );
