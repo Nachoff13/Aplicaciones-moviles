56c03bca4c716c4004ddba93531f9132ab3cb5f9
Nacho <nachofrangolini@hotmail.com>
2024-11-03 17:42:45 -0300

    feat: agrego los archivos expo a la configuracion

--- farmacias-de-turno/app.config.js
+++ farmacias-de-turno/app.config.js
@@ -29,7 +29,7 @@ export default {
         "ACCESS_FINE_LOCATION",
         "ACCESS_COARSE_LOCATION"
       ],
-      package: "com.anonymous.farmaciasdeturno" // Agrega esta línea
+      package: "com.anonymous.farmaciasdeturno"
     },
     web: {
       bundler: "metro",
@@ -37,7 +37,13 @@ export default {
       favicon: "./assets/images/favicon.png"
     },
     extra: {
-      googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY
+      googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+      firebaseApiKey: process.env.FIREBASE_API_KEY,
+      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppId: process.env.FIREBASE_APP_ID
     },
     plugins: [
       [
