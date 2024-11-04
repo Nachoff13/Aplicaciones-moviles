59dc29ea81454e2f59ec90db675110f9135eded2
Manuel Morullo <manuelmorullo@hotmail.com>
2024-11-04 05:28:02 -0300

    fix: corregi problemas estilos de la pantalla progressHabit

--- Trabajos Practicos/TP3/Habitos/components/ThemeContext.tsx
+++ Trabajos Practicos/TP3/Habitos/components/ThemeContext.tsx
@@ -1,13 +1,20 @@
 import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
 import { useColorScheme as useSystemColorScheme } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Colors } from '../constants/Colors'; // Asegúrate de importar Colors
+import { ViewStyle, TextStyle } from 'react-native';
 
 type Theme = 'light' | 'dark';
+
+interface ThemeStyles {
+  container: ViewStyle;
+  text: TextStyle;
+  recordContainer: ViewStyle;
+}
 
 interface ThemeContextProps {
   theme: Theme;
   toggleTheme: () => void;
+  currentTheme: ThemeStyles;
 }
 
 const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
@@ -15,6 +22,18 @@ const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
 export const ThemeProvider = ({ children }: { children: ReactNode }) => {
   const systemColorScheme = useSystemColorScheme();
   const [theme, setTheme] = useState<Theme>('light');
+
+  const currentTheme: ThemeStyles = theme === 'light'
+    ? {
+        container: { backgroundColor: 'white' },
+        text: { color: 'black' },
+        recordContainer: { backgroundColor: '#f9f9f9' },
+      }
+    : {
+        container: { backgroundColor: '#121212' },
+        text: { color: 'white' },
+        recordContainer: { backgroundColor: '#1E1E1E' },
+      };
 
   useEffect(() => {
     const loadTheme = async () => {
@@ -36,7 +55,7 @@ export const ThemeProvider = ({ children }: { children: ReactNode }) => {
   };
 
   return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+    <ThemeContext.Provider value={{ theme, toggleTheme, currentTheme }}>
       {children}
     </ThemeContext.Provider>
   );
