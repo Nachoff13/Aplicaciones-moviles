8aabf816d8bdefb4135254248cf8ffb4ecb9b396
Manuel Morullo <manuelmorullo@hotmail.com>
2024-11-03 22:55:44 -0300

    feat: agrego horario y dias al habito y logica para cargarlos

--- Trabajos Practicos/TP3/Habitos/app/(tabs)/addHabit.tsx
+++ Trabajos Practicos/TP3/Habitos/app/(tabs)/addHabit.tsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { View, TextInput, Button, StyleSheet, TouchableOpacity, Text, Alert } from 'react-native';
+import { View, TextInput, Alert, StyleSheet, TouchableOpacity, Text, ScrollView } from 'react-native';
 import * as SQLite from 'expo-sqlite';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { auth } from '../../firebaseConfig'; 
+import { auth } from '../../firebaseConfig';
+import { CheckBox } from 'react-native-elements';
 import { Picker } from '@react-native-picker/picker';
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 type RootStackParamList = {
   AddHabit: undefined;
@@ -14,12 +16,17 @@ type RootStackParamList = {
 type AddHabitScreenNavigationProp = StackNavigationProp<RootStackParamList, 'AddHabit'>;
 
 const AddHabit = () => {
-  const [habitName, setHabitName] = useState<string>('');
-  const [habitImportance, setHabitImportance] = useState<string>('');
-  const [habitDescription, setHabitDescription] = useState<string>('');
-  const [habitActive, setHabitActive] = useState<number>(1);
+  const [habitName, setHabitName] = useState('');
+  const [habitImportance, setHabitImportance] = useState('');
+  const [habitDescription, setHabitDescription] = useState('');
+  const [habitActive, setHabitActive] = useState(1);
+  const [selectedDays, setSelectedDays] = useState<string[]>([]);
   const navigation = useNavigation<AddHabitScreenNavigationProp>();
   const [db, setDb] = useState<SQLite.SQLiteDatabase | null>(null);
+  const [startTime, setStartTime] = useState(new Date());
+  const [endTime, setEndTime] = useState(new Date());
+  const [isStartPickerVisible, setStartPickerVisible] = useState(false);
+  const [isEndPickerVisible, setEndPickerVisible] = useState(false);
 
   useEffect(() => {
     const initializeDatabase = async () => {
@@ -28,7 +35,7 @@ const AddHabit = () => {
       //await database.execAsync("DROP TABLE IF EXISTS habits;");
 
       await database.execAsync(
-        "CREATE TABLE IF NOT EXISTS habits (id INTEGER PRIMARY KEY NOT NULL, name TEXT, importance TEXT, description TEXT, active INTEGER DEFAULT 1, user_id TEXT);"
+        "CREATE TABLE IF NOT EXISTS habits (id INTEGER PRIMARY KEY NOT NULL, name TEXT, importance TEXT, description TEXT, active INTEGER DEFAULT 1, user_id TEXT, days TEXT, start_time TEXT, end_time TEXT);"
       );
     };
 
@@ -46,7 +53,6 @@ const AddHabit = () => {
       return;
     }
 
-    // Obtengo el UID del usuario autenticado
     const currentUser = auth.currentUser;
     if (!currentUser) {
       Alert.alert('Error', 'No se encontró el usuario autenticado');
@@ -56,11 +62,19 @@ const AddHabit = () => {
 
     try {
       const result = await db.runAsync(
-        'INSERT INTO habits (name, importance, description, active, user_id) VALUES (?, ?, ?, ?, ?)',
-        [habitName, habitImportance, habitDescription, habitActive, userId]
+        'INSERT INTO habits (name, importance, description, active, user_id, days, start_time, end_time) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+        [
+          habitName,
+          habitImportance,
+          habitDescription,
+          habitActive,
+          userId,
+          selectedDays.join(','),
+          startTime.toTimeString().split(' ')[0].slice(0, 5), // Formato "HH:mm"
+          endTime.toTimeString().split(' ')[0].slice(0, 5),
+        ]
       );
 
-      
       if (result.lastInsertRowId) {
         Alert.alert('Éxito', `Hábito agregado con éxito.`);
         db.closeAsync();
@@ -73,8 +87,26 @@ const AddHabit = () => {
     }
   };
 
+  const toggleDay = (day: string) => {
+    setSelectedDays((prevDays) =>
+      prevDays.includes(day)
+        ? prevDays.filter((d) => d !== day)
+        : [...prevDays, day]
+    );
+  };
+
+  const handleStartConfirm = (time: Date) => {
+    setStartTime(time);
+    setStartPickerVisible(false);
+  };
+
+  const handleEndConfirm = (time: Date) => {
+    setEndTime(time);
+    setEndPickerVisible(false);
+  };
+
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <Text style={styles.title}>Agregar Hábitos</Text>
       
       <Text style={styles.label}>Nombre</Text>
@@ -86,18 +118,16 @@ const AddHabit = () => {
       />
       
       <Text style={styles.label}>Importancia</Text>
-      <View style={styles.pickerContainer}>
-        <Picker
-          selectedValue={habitImportance}
-          onValueChange={(itemValue) => setHabitImportance(itemValue)}
-          style={styles.picker}
-        >
-          <Picker.Item label="Seleccione una importancia" value="" />
-          <Picker.Item label="Baja" value="Baja" />
-          <Picker.Item label="Media" value="Media" />
-          <Picker.Item label="Alta" value="Alta" />
-        </Picker>
-      </View>
+      <Picker
+        selectedValue={habitImportance}
+        onValueChange={(itemValue) => setHabitImportance(itemValue)}
+        style={styles.picker}
+      >
+        <Picker.Item label="Seleccione una importancia" value="" />
+        <Picker.Item label="Baja" value="Baja" />
+        <Picker.Item label="Media" value="Media" />
+        <Picker.Item label="Alta" value="Alta" />
+      </Picker>
 
       <Text style={styles.label}>Descripción</Text>
       <TextInput
@@ -107,10 +137,42 @@ const AddHabit = () => {
         onChangeText={setHabitDescription}
       />
 
+      <Text style={styles.label}>Días de la semana</Text>
+      {['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'].map((day) => (
+        <CheckBox
+          key={day}
+          title={day}
+          checked={selectedDays.includes(day)}
+          onPress={() => toggleDay(day)}
+        />
+      ))}
+
+      <Text style={styles.label}>Horario de Inicio</Text>
+      <TouchableOpacity style={styles.buttonHour} onPress={() => setStartPickerVisible(true)}>
+        <Text style={styles.buttonTextHour}>{startTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</Text>
+      </TouchableOpacity>
+      <DateTimePickerModal
+        isVisible={isStartPickerVisible}
+        mode="time"
+        onConfirm={handleStartConfirm}
+        onCancel={() => setStartPickerVisible(false)}
+      />
+
+      <Text style={styles.label}>Horario de Fin</Text>
+      <TouchableOpacity style={styles.buttonHour} onPress={() => setEndPickerVisible(true)}>
+        <Text style={styles.buttonTextHour}>{endTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</Text>
+      </TouchableOpacity>
+      <DateTimePickerModal
+        isVisible={isEndPickerVisible}
+        mode="time"
+        onConfirm={handleEndConfirm}
+        onCancel={() => setEndPickerVisible(false)}
+      />
+
       <TouchableOpacity style={styles.button} onPress={handleAddHabit}>
         <Text style={styles.buttonText}>Agregar Hábito</Text>
       </TouchableOpacity>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -137,18 +199,13 @@ const styles = StyleSheet.create({
   label: {
     fontSize: 16,
     marginBottom: 8,
-  },
-  pickerContainer: {
-    borderColor: '#ddd',
-    borderWidth: 1,
-    borderRadius: 5,
-    marginBottom: 20,
-    backgroundColor: '#fff',
+
   },
   picker: {
     height: 50,
     width: '100%',
     borderRadius: 5,
+    backgroundColor: '#fff',
   },
   button: {
     backgroundColor: '#4285f4',
@@ -160,9 +217,23 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.2,
     shadowRadius: 2,
     elevation: 5,
+    marginBottom: 25,
   },
   buttonText: {
     color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  buttonHour: {
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    padding: 15,
+    borderColor: '#C3C3C3',
+    borderWidth: 2,
+    marginBottom: 15,
+  },
+  buttonTextHour: {
+    color: '#000000',
     fontSize: 18,
     fontWeight: 'bold',
   },
