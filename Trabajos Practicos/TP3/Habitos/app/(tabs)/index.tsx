9406abbd36d455984950b7b43bbb2849d567010c
Juan Ignacio Frangolini <nachofrangolini@hotmail.com>
2024-11-04 20:33:06 -0300

    fix: envuelvo la app con el modo oscuro/claro

--- Trabajos Practicos/TP3/Habitos/app/(tabs)/index.tsx
+++ Trabajos Practicos/TP3/Habitos/app/(tabs)/index.tsx
@@ -1,7 +1,49 @@
+import React, { useState } from 'react';
+import { Text, StyleSheet, View, Button } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 import { Redirect } from 'expo-router';
 
 const Index = () => {
-  return <Redirect href="/login" />;
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  const themeTextStyle = isDarkMode ? styles.darkThemeText : styles.lightThemeText;
+  const themeContainerStyle = isDarkMode ? styles.darkContainer : styles.lightContainer;
+
+  return (
+    <View style={[styles.container, themeContainerStyle]}>
+      <Text style={[styles.text, themeTextStyle]}>Color scheme: {isDarkMode ? 'dark' : 'light'}</Text>
+      <Button title="Toggle Theme" onPress={toggleTheme} />
+      <StatusBar />
+      <Redirect href="/login" />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+  },
+  lightContainer: {
+    backgroundColor: '#d0d0c0',
+  },
+  darkContainer: {
+    backgroundColor: '#242c40',
+  },
+  lightThemeText: {
+    color: '#242c40',
+  },
+  darkThemeText: {
+    color: '#d0d0c0',
+  },
+});
 
 export default Index;
