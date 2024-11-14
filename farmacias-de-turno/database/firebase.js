3c3d7225e446910cc885d66799c5582e2a8567da
Juan Ignacio Frangolini <nachofrangolini@hotmail.com>
2024-11-14 01:42:47 -0300

    fix: arreglo de importancios de firebase

--- farmacias-de-turno/database/firebase.js
+++ farmacias-de-turno/database/firebase.js
@@ -1,16 +1,33 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+} from '@env';
+
+console.log('FIREBASE_API_KEY:', FIREBASE_API_KEY);
+console.log('FIREBASE_AUTH_DOMAIN:', FIREBASE_AUTH_DOMAIN);
+console.log('FIREBASE_PROJECT_ID:', FIREBASE_PROJECT_ID);
+console.log('FIREBASE_STORAGE_BUCKET:', FIREBASE_STORAGE_BUCKET);
+console.log('FIREBASE_MESSAGING_SENDER_ID:', FIREBASE_MESSAGING_SENDER_ID);
+console.log('FIREBASE_APP_ID:', FIREBASE_APP_ID);
 
 export const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+
+console.log('Firebase inicializado:', app);
 
 export { db };
