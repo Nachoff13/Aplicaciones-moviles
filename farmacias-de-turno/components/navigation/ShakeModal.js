934a72916f0245ebc1cf8014303717d0b12b1b7d
Sebastian Gonzales <sebastian.gt.2002@gmail.com>
2024-11-14 01:21:27 -0300

    Agrego modo oscuro para el modal

--- farmacias-de-turno/components/navigation/ShakeModal.js
+++ farmacias-de-turno/components/navigation/ShakeModal.js
@@ -1,33 +1,71 @@
 import React, { useState } from 'react';
-import { View, Modal, Button, StyleSheet, TextInput } from 'react-native';
+import {
+  View,
+  Modal,
+  Button,
+  StyleSheet,
+  TextInput,
+  useColorScheme,
+} from 'react-native';
 import { ThemedText } from '../ThemedText';
+import { ThemedView } from '../ThemedView';
+import { useNavigation } from '@react-navigation/native';
 
 export default function ShakeModal({ visible, onClose }) {
+  let colorScheme = useColorScheme();
+
   const [password, setPassword] = useState('');
+  const navigation = useNavigation();
+
+  const handlePasswordSubmit = () => {
+    if (password === '123456') {
+      onClose();
+      setPassword('');
+      navigation.navigate('admin');
+    } else {
+      alert('Contraseña incorrecta');
+      setPassword('');
+    }
+  };
+
+  const handleOnClose = () => {
+    setPassword('');
+    onClose();
+  };
 
   return (
-    <Modal
-      animationType="slide"
-      transparent={true}
-      visible={visible}
-      onRequestClose={onClose}
-    >
-      <View style={styles.modalContainer}>
-        <View style={styles.modalView}>
-          <ThemedText style={styles.modalText}>
-            Ingrese la contraseña del administrador
-          </ThemedText>
-          <TextInput
-            style={styles.input}
-            placeholder="Contraseña"
-            secureTextEntry={true}
-            value={password}
-            onChangeText={setPassword}
-          />
-          <Button onPress={onClose} title="Cerrar" />
-        </View>
-      </View>
-    </Modal>
+    <ThemedView>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={visible}
+        onRequestClose={onClose}
+      >
+        <ThemedView style={styles.modalContainer}>
+          <ThemedView
+            style={
+              (styles.modalViewLight,
+              colorScheme === 'dark' && styles.modalViewDark)
+            }
+          >
+            <ThemedText style={styles.modalText}>
+              Ingrese la contraseña del administrador
+            </ThemedText>
+            <TextInput
+              style={styles.input}
+              placeholder="Contraseña"
+              secureTextEntry={true}
+              value={password}
+              onChangeText={setPassword}
+            />
+            <ThemedView style={styles.buttonContainer}>
+              <Button onPress={handlePasswordSubmit} title="Enviar" />
+              <Button onPress={handleOnClose} title="Cerrar" />
+            </ThemedView>
+          </ThemedView>
+        </ThemedView>
+      </Modal>
+    </ThemedView>
   );
 }
 
@@ -38,9 +76,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: 'rgba(0, 0, 0, 0.5)', // Fondo semitransparente
   },
-  modalView: {
+  modalViewLight: {
     margin: 20,
     backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  modalViewDark: {
+    margin: 20,
+    backgroundColor: '#151718',
     borderRadius: 20,
     padding: 35,
     alignItems: 'center',
@@ -58,12 +111,16 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   input: {
-    height: 40,
+    height: 30,
     borderColor: 'gray',
     borderWidth: 1,
     borderRadius: 5,
     paddingHorizontal: 10,
     marginBottom: 20,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     width: '100%',
   },
 });
