173edfdfd3f813629dac810569cd18c63fe316ef
Sebastian Gonzales <sebastian.gt.2002@gmail.com>
2024-11-16 11:09:53 -0300

    feat: Agrego más items que traiga la api

--- farmacias-de-turno/utils/globalApi.js
+++ farmacias-de-turno/utils/globalApi.js
@@ -4,16 +4,19 @@ import Constants from 'expo-constants';
 const BASE_URL = 'https://places.googleapis.com/v1/places:searchNearby';
 const API_KEY = Constants.expoConfig?.extra?.googleMapsApiKey;
 const config = {
-    headers:{
-        'Content-Type': 'application/json',
-        'X-Goog-Api-Key': API_KEY,
-        'X-Goog-FieldMask': ['places.displayName',
-        'places.formattedAddress',
-        'places.shortFormattedAddress', 
-        'places.location', 
-        'places.photos']
-    }
-}
-const NewNearbyPlace = (data) => axios.post(BASE_URL,data,config);
+  headers: {
+    'Content-Type': 'application/json',
+    'X-Goog-Api-Key': API_KEY,
+    'X-Goog-FieldMask': [
+      'places.displayName',
+      'places.formattedAddress',
+      'places.shortFormattedAddress',
+      'places.location',
+      'places.photos',
+      'places.currentOpeningHours',
+    ],
+  },
+};
+const NewNearbyPlace = (data) => axios.post(BASE_URL, data, config);
 
 export default { NewNearbyPlace, API_KEY };
