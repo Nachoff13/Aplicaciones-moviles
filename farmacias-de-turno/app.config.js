67072a0ed943b36aa58280cff443dbafd43d7e41
Manuel Morullo <manuelmorullo@hotmail.com>
2024-11-17 23:29:36 -0300

    fix: arreglo variables de entorno

--- farmacias-de-turno/app.config.js
+++ farmacias-de-turno/app.config.js
@@ -1,4 +1,4 @@
-import 'dotenv/config';
+
 
 export default {
   expo: {
@@ -42,13 +42,13 @@ export default {
       favicon: './assets/images/favicon.png',
     },
     extra: {
-      googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
-      firebaseApiKey: process.env.FIREBASE_API_KEY,
-      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
-      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      firebaseAppId: process.env.FIREBASE_APP_ID,
+      googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+      firebaseApiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+      firebaseAuthDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      firebaseProjectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+      firebaseStorageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
     },
     plugins: [
       [
